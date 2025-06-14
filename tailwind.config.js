5b82003260397fd37e434245d6ef4d31027cd1a1
Om Prajapati <omrajendrakumar2004@gmail.com>
2025-06-14 10:48:32 +0530

    Fonts Problem Fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,18 @@
 module.exports = {
     content: [
         "./index.html",
-        "./src/**/*.{js,ts,jsx,tsx}",
+        "./src//*.{js,ts,jsx,tsx}",
     ],
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                mont: ['Montserrat', 'ui-sans-serif', 'system-ui'],
+                syne: ['Syne', 'ui-sans-serif', 'system-ui'],
+                oswald: ['Oswald', 'ui-sans-serif', 'system-ui'],
+                dastin: ['Dastin', 'ui-sans-serif', 'system-ui'],
+                cav: ['CaviarDreams', 'ui-sans-serif', 'system-ui'],
+            },
+        },
     },
     plugins: [],
 };
